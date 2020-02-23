3b6529aaa18def0fa768c5fa1d3e15d2f6b5855d
Gun1dex <baptisteb.benoit@hotmail.fr>
2020-02-23 18:31:50 +0100

    Affichage inventaire des joueurs et placement et suppression des pions dans le tableau.

--- js/Player.js
+++ js/Player.js
@@ -9,6 +9,21 @@ class Player {
         }
     }
 
+    afficherInventaire(numJoueur) {
+        if(numJoueur != 1 && numJoueur != 2) {
+            return;
+        }
+        let enfants = document.getElementById("div-joueur"+numJoueur);
+
+        for (let i = 0; i < this.pions.length; i++) {
+            if(!this.pions[i].estPlace()) {
+                let image = document.createElement("img");
+                image.src = this.pions[i].getImageToDisplay();
+                enfants.appendChild(image);
+            }
+        }
+    }
+
     /**
      * Retourne le tableau de pions.
      */
