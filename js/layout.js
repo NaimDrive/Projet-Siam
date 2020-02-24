f6cb1d25b125c7be6997e628e7213c5dbd217585
NaimDrive <naimdrive98@gmail.com>
2020-02-24 12:20:32 +0100

    ajout modal

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $.get("/layout/header_logged.html",
+    $.get("/layout/header_unlogged.html",
         function(response) {
             $("body").prepend(response);
         }
