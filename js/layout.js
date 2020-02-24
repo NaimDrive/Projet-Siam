45f920a6c0f5cb1c1acdf0f24405ba0a8fd72d55
NaimDrive <naimdrive98@gmail.com>
2020-02-24 15:13:45 +0100

    ajout form

--- js/layout.js
+++ js/layout.js
@@ -2,7 +2,6 @@ function initLayout(session) {
     $(document).ready(function () {
 
         if(Array.isArray(session) && session.length == 0) {
-            console.log("Est vide !");
             
             $.get("/layout/header_unlogged.html",
                 function(response) {
@@ -11,7 +10,6 @@ function initLayout(session) {
             
                 );
         } else {
-            console.log("En ligne !");
             
             $.get("/layout/header_logged.html",
                 function(response) {
@@ -28,15 +26,5 @@ function initLayout(session) {
                 $("#footer").insertAfter("body>div:last");
             }
         );
-
-        /*$.ajax({
-            type: "POST",
-            url: "/ajax/test.ajax.php",
-            // data: "data",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-        }).always(function(response) {
-            console.log("rep : " + response);
-        });*/
     });
 }
